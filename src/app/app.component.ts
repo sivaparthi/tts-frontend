ada4e0d5e08cc074f1217a6ab6896e486e94e41e
Siva <sivaparthi1989@gmail.com>
2024-10-20 15:36:21 +0530

    added flaticon and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ import { ToggleButtonComponent } from './toggle-button/toggle-button.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'tts';
+  title = 'Your Friend';
 }
